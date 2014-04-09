1b2aaaa89332dd99f6eb2bddc2a9aa0515691b0f
Shahar Talmi <shahar.talmi@gmail.com>
2014-04-09 16:27:34 +0300

    fix(regexp): eat also escaped charcters inside string

--- test.js
+++ test.js
@@ -13,4 +13,5 @@ it('should convert matching double-quotes to single-quotes', function () {
 	assert.equal(s('\'bar\' "foo" \'baz\''), '\'bar\' \'foo\' \'baz\'');
 	assert.equal(s('\\\"foo\\\"'), '\'foo\'');
 	assert.equal(s(JSON.stringify({'a': '<a href="addr">'})), '{\'a\':\'<a href="addr">\'}');
+	assert.equal(s(JSON.stringify({'a': 'aa\n<a href="addr">'})), '{\'a\':\'aa\\n<a href="addr">\'}');
 });
