ebef22381ced4b54f692883a64d5063c9a2c1a89
Guy Rotem <guy.rotem@wix.com>
2015-02-16 16:04:13 +0200

    fix two consecutive apostrophes escaping ('') bug

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 module.exports = function (str) {
+  function escapeIfNotPrecededByEscapeChar(escapeChar, match, offset, string) { return string[offset - 1] === escapeChar ? match : escapeChar + match;}
 	return str.replace(/(?:\\*)?"([^"\\]*\\.)*[^"]*"/g, function (match) {
 		return match
 			.replace(/\\"/g, '"')            // unescape double-quotes
-			.replace(/([^\\])'/g, '$1\\\'')  // escape single-quotes
+			.replace(/'/g, escapeIfNotPrecededByEscapeChar.bind(null, '\\'))
 			.replace(/^"|"$/g, '\'');        // convert
 	});
 };
