358c92b8c96bfa9a7abd42578b34efa9ac2127c5
Guy Rotem <guy.rotem@wix.com>
2015-02-16 16:09:30 +0200

    fix indentation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function (str) {
-  function escapeIfNotPrecededByEscapeChar(escapeChar, match, offset, string) { return string[offset - 1] === escapeChar ? match : escapeChar + match;}
+	function escapeIfNotPrecededByEscapeChar(escapeChar, match, offset, string) { return string[offset - 1] === escapeChar ? match : escapeChar + match;}
 	return str.replace(/(?:\\*)?"([^"\\]*\\.)*[^"]*"/g, function (match) {
 		return match
 			.replace(/\\"/g, '"')            // unescape double-quotes
