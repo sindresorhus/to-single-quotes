e8f90a0ccc59a96467440df38a7daa4604532db8
Oleg Gaidarenko <markelog@gmail.com>
2015-05-31 19:23:12 +0200

    Close #7 PR: Deal with backslashes. Fixes #6

--- test.js
+++ test.js
@@ -14,5 +14,13 @@ it('should convert matching double-quotes to single-quotes', function () {
 	assert.equal(s('\\\"foo\\\"'), '\'foo\'');
 	assert.equal(s(JSON.stringify({'a': '<a href="addr">'})), '{\'a\':\'<a href="addr">\'}');
 	assert.equal(s(JSON.stringify({'a': 'aa\n<a href="addr">'})), '{\'a\':\'aa\\n<a href="addr">\'}');
- 	assert.equal(s(JSON.stringify({"a": "b''c" })), "{'a':'b\\\'\\\'c'}");
+	assert.equal(s(JSON.stringify({"a": "b''c" })), "{'a':'b\\\'\\\'c'}");
+});
+
+it('should convert matching double-quotes to single-quotes despite backslashes', function () {
+	assert.equal(s('"1\\\'"'), '\'1\\\\\'\'', 'First sequence in the string');
+	assert.equal(s('"\\\'"'), '\'\\\\\'\'', 'Not the first sequence in the string');
+	assert.equal(s('"\\\\\'"'), '\'\\\\\\\'\'', 'Double backslash');
+	assert.equal(s('"\\\\\' \\\\\'"'), "'\\\\\\' \\\\\\''", 'Repetition');
+	assert.equal(s('"\\\\n \\\\\'"'), "'\\\\n \\\\\\''", 'With another backslash character');
 });
