467ba535f0f33b059ebb987ea132d77ad53c3754
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-04 00:34:20 +0700

    add XO
    https://github.com/sindresorhus/xo

--- test.js
+++ test.js
@@ -1,26 +1,27 @@
+/* eslint-env mocha */
 'use strict';
 var assert = require('assert');
-var s = require('./');
+var fn = require('./');
 
 it('should convert matching double-quotes to single-quotes', function () {
-	assert.equal(s(''), '');
-	assert.equal(s('foo'), 'foo');
-	assert.equal(s('\'\''), '\'\'');
-	assert.equal(s('""'), '\'\'');
-	assert.equal(s('\'foo\''), '\'foo\'');
-	assert.equal(s('"foo"'), '\'foo\'');
-	assert.equal(s('bar "foo" baz'), 'bar \'foo\' baz');
-	assert.equal(s('\'bar\' "foo" \'baz\''), '\'bar\' \'foo\' \'baz\'');
-	assert.equal(s('\\\"foo\\\"'), '\'foo\'');
-	assert.equal(s(JSON.stringify({'a': '<a href="addr">'})), '{\'a\':\'<a href="addr">\'}');
-	assert.equal(s(JSON.stringify({'a': 'aa\n<a href="addr">'})), '{\'a\':\'aa\\n<a href="addr">\'}');
-	assert.equal(s(JSON.stringify({"a": "b''c" })), "{'a':'b\\\'\\\'c'}");
+	assert.equal(fn(''), '');
+	assert.equal(fn('foo'), 'foo');
+	assert.equal(fn('\'\''), '\'\'');
+	assert.equal(fn('""'), '\'\'');
+	assert.equal(fn('\'foo\''), '\'foo\'');
+	assert.equal(fn('"foo"'), '\'foo\'');
+	assert.equal(fn('bar "foo" baz'), 'bar \'foo\' baz');
+	assert.equal(fn('\'bar\' "foo" \'baz\''), '\'bar\' \'foo\' \'baz\'');
+	assert.equal(fn('\\\"foo\\\"'), '\'foo\'');
+	assert.equal(fn(JSON.stringify({a: '<a href="addr">'})), '{\'a\':\'<a href="addr">\'}');
+	assert.equal(fn(JSON.stringify({a: 'aa\n<a href="addr">'})), '{\'a\':\'aa\\n<a href="addr">\'}');
+	assert.equal(fn(JSON.stringify({a: 'b\'\'c'})), '{\'a\':\'b\\\'\\\'c\'}');
 });
 
 it('should convert matching double-quotes to single-quotes despite backslashes', function () {
-	assert.equal(s('"1\\\'"'), '\'1\\\\\'\'', 'First sequence in the string');
-	assert.equal(s('"\\\'"'), '\'\\\\\'\'', 'Not the first sequence in the string');
-	assert.equal(s('"\\\\\'"'), '\'\\\\\\\'\'', 'Double backslash');
-	assert.equal(s('"\\\\\' \\\\\'"'), "'\\\\\\' \\\\\\''", 'Repetition');
-	assert.equal(s('"\\\\n \\\\\'"'), "'\\\\n \\\\\\''", 'With another backslash character');
+	assert.equal(fn('"1\\\'"'), '\'1\\\\\'\'', 'First sequence in the string');
+	assert.equal(fn('"\\\'"'), '\'\\\\\'\'', 'Not the first sequence in the string');
+	assert.equal(fn('"\\\\\'"'), '\'\\\\\\\'\'', 'Double backslash');
+	assert.equal(fn('"\\\\\' \\\\\'"'), '\'\\\\\\\' \\\\\\\'\'', 'Repetition');
+	assert.equal(fn('"\\\\n \\\\\'"'), '\'\\\\n \\\\\\\'\'', 'With another backslash character');
 });
