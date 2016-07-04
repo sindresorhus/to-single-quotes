98bb823b2cef2589e8a78bb48e59672eaa8fb0ac
Serhan Apaydın <zerhan@gmail.com>
2016-07-05 01:32:58 +0200

    Fix backslash bug
    This PR aims to fix #11. I've also added tests, but still I'm not 100% sure that it won't break anything. I tried to make sure that the replacement phase titled "unescape double-quotes" does not mess with the escaped backslashes that are followed with a double quote.
    
    Testing this with inline strings was so difficult for me, so created a folder named "tests" and put all the relevant files into that folder.
    
    Closes #12

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import test from 'ava';
 import fn from './';
 
@@ -10,11 +11,13 @@ test('convert matching double-quotes to single-quotes', t => {
 	t.is(fn('"foo"'), '\'foo\'');
 	t.is(fn('bar "foo" baz'), 'bar \'foo\' baz');
 	t.is(fn('\'bar\' "foo" \'baz\''), '\'bar\' \'foo\' \'baz\'');
-	t.is(fn('\\\"foo\\\"'), '\'foo\'');
 	t.is(fn(JSON.stringify({a: '<a href="addr">'})), '{\'a\':\'<a href="addr">\'}');
 	t.is(fn(JSON.stringify({a: 'aa\n<a href="addr">'})), '{\'a\':\'aa\\n<a href="addr">\'}');
 	t.is(fn(JSON.stringify({a: 'b\'\'c'})), '{\'a\':\'b\\\'\\\'c\'}');
-	t.end();
+});
+
+test('convert matching double-quotes to single-quotes but do not touch escaped backslashes', t => {
+	t.is(fn(fs.readFileSync('./tests/do-not-touch-escaped-backslashes/input.txt', 'utf-8')), fs.readFileSync('./tests/do-not-touch-escaped-backslashes/expected-result.txt', 'utf-8'));
 });
 
 test('convert matching double-quotes to single-quotes despite backslashes', t => {
@@ -23,5 +26,4 @@ test('convert matching double-quotes to single-quotes despite backslashes', t =>
 	t.is(fn('"\\\\\'"'), '\'\\\\\\\'\'', 'Double backslash');
 	t.is(fn('"\\\\\' \\\\\'"'), '\'\\\\\\\' \\\\\\\'\'', 'Repetition');
 	t.is(fn('"\\\\n \\\\\'"'), '\'\\\\n \\\\\\\'\'', 'With another backslash character');
-	t.end();
 });
