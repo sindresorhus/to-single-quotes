57397ba388b9d6a9190d5a0a9f698f0def6fe1c7
Rui Neves <rpmrneves@gmail.com>
2017-01-19 23:52:32 +0000

    Update parser to process escape and backslashes. Update state changes. All tests passed.

--- index.js
+++ index.js
@@ -3,18 +3,48 @@ module.exports = function (str) {
 	var result = "";
 	var x, y, len = str.length;
 	var betweenQuotes = false;
+	var QuoteChar;
 	for(x = 0; x < len; x++) {
+		//FOUND DOUBLE-QUOTE OR SINGLE-QUOTE
 		if(str[x] === '"' || str[x] === '\'') {
-
+			//IF NOT PROCESSING IN BETWEEN QUOTES
 			if(!betweenQuotes) {
+				QuoteChar = str[x];
 				betweenQuotes = true;
 				result+= '\'';
 			}
+			//IF PROCESSING IN BETWEEN QUOTES
 			else {
+				//CLOSE QUOTES
+				if(QuoteChar == str[x]) {
 					result+= '\'';
-				betweenQuotes = false;
+					betweenQuotes = false;
+				}
+				//STILL INSIDE QUOTES
+				else
+					result+= '\\\'';
 			}
 
+		}
+		//IF ESCAPE CHARACTER IS FOUND AND DOUBLE OR SINGLE QUOTE AFTER
+		else if(str[x] === '\\' && (str[x+1] === '\'' || str[x+1] === '"')) {
+			//ESCAPE + SINGLE QUOTE
+			if(str[x+1] === '\'') {
+				result+= '\\\\\'';
+				x++;
+			}
+			//ESCAPE + DOUBLE QUOTE
+			else if(str[x+1] === '"') {
+				result+= '"';
+				x++;
+			}
+			else
+				result+= str[x];
+		}
+		//BACKSLASHES --> DON'T TOUCHE THEM
+		else if(str[x] === '\\' && str[x+1] === '\\') {
+			result+='\\\\';
+			x++;
 		}
 		else
 			result+= str[x];
--- test.js
+++ test.js
@@ -12,11 +12,11 @@ test('convert matching double-quotes to single-quotes', t => {
 	t.is(m('bar "foo" baz'), 'bar \'foo\' baz');
 	t.is(m('\'bar\' "foo" \'baz\''), '\'bar\' \'foo\' \'baz\'');
 	t.is(m(JSON.stringify({a: '<a href="addr">'})), '{\'a\':\'<a href="addr">\'}');
-	/*t.is(m(JSON.stringify({a: 'aa\n<a href="addr">'})), '{\'a\':\'aa\\n<a href="addr">\'}');
-	t.is(m(JSON.stringify({a: 'b\'\'c'})), '{\'a\':\'b\\\'\\\'c\'}');*/
+	t.is(m(JSON.stringify({a: 'aa\n<a href="addr">'})), '{\'a\':\'aa\\n<a href="addr">\'}');
+	t.is(m(JSON.stringify({a: 'b\'\'c'})), '{\'a\':\'b\\\'\\\'c\'}');
 });
 
-/*test('convert matching double-quotes to single-quotes but do not touch escaped backslashes', t => {
+test('convert matching double-quotes to single-quotes but do not touch escaped backslashes', t => {
 	t.is(m(fs.readFileSync('fixtures/dont-touch-escaped-backslashes/fixture.txt', 'utf8')), fs.readFileSync('fixtures/dont-touch-escaped-backslashes/expected.txt', 'utf8'));
 });
 
@@ -26,4 +26,4 @@ test('convert matching double-quotes to single-quotes despite backslashes', t =>
 	t.is(m('"\\\\\'"'), '\'\\\\\\\'\'', 'Double backslash');
 	t.is(m('"\\\\\' \\\\\'"'), '\'\\\\\\\' \\\\\\\'\'', 'Repetition');
 	t.is(m('"\\\\n \\\\\'"'), '\'\\\\n \\\\\\\'\'', 'With another backslash character');
-});*/
+});
