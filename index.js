0562706f4b1161b86dcb7f15703985ea5397465f
Rui Neves <rpmrneves@gmail.com>
2017-01-19 23:09:10 +0000

    Update to string processing. First 9 tests passed successfully.

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 'use strict';
 module.exports = function (str) {
-	return str.replace(/(?:\\*)?"([^"\\]*\\.)*[^"]*"/g, function (match) {
-		return match
-			// unescape double-quotes
-			.replace(/([^\\]|^)\\"/g, '$1"')
-			// escape escapes
-			.replace(/(^|[^\\])(\\+)'/g, '$1$2\\\'')
-			// escape single-quotes - round 1
-			.replace(/([^\\])'/g, '$1\\\'')
-			// escape single-quotes - round 2 (for consecutive single-quotes)
-			.replace(/([^\\])'/g, '$1\\\'')
-			// convert
-			.replace(/^"|"$/g, '\'');
-	});
+	var result = "";
+	var x, y, len = str.length;
+	var betweenQuotes = false;
+	for(x = 0; x < len; x++) {
+		if(str[x] === '"' || str[x] === '\'') {
+
+			if(!betweenQuotes) {
+				betweenQuotes = true;
+				result+= '\'';
+			}
+			else {
+					result+= '\'';
+				betweenQuotes = false;
+			}
+
+		}
+		else
+			result+= str[x];
+	}
+	return result;
 };
