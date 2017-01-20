20960f7b6cfdcd4e04d1e6819d8f4437cc23aa4a
Rui Neves <rpmrneves@gmail.com>
2017-01-20 23:02:48 +0000

    Fix linter-xo issues

--- index.js
+++ index.js
@@ -1,53 +1,47 @@
 'use strict';
 module.exports = function (str) {
-	var result = "";
-	var x, y, len = str.length;
+	var result = '';
+	var x;
+	var len = str.length;
 	var betweenQuotes = false;
 	var QuoteChar;
-	for(x = 0; x < len; x++) {
-		//FOUND DOUBLE-QUOTE OR SINGLE-QUOTE
-		if(str[x] === '"' || str[x] === '\'') {
-			//IF NOT PROCESSING IN BETWEEN QUOTES
-			if(!betweenQuotes) {
+	for (x = 0; x < len; x++) {
+		// FOUND DOUBLE-QUOTE OR SINGLE-QUOTE
+		if (str[x] === '"' || str[x] === '\'') {
+			// IF NOT PROCESSING IN BETWEEN QUOTES
+			if (betweenQuotes === false) {
 				QuoteChar = str[x];
 				betweenQuotes = true;
-				result+= '\'';
+				result += '\'';
+			} else if (QuoteChar === str[x]) {
+				// IF PROCESSING IN BETWEEN QUOTES
+				// CLOSE QUOTES
+				result += '\'';
+				betweenQuotes = false;
+			}	else {
+					// STILL INSIDE QUOTES
+				result += '\\\'';
 			}
-			//IF PROCESSING IN BETWEEN QUOTES
-			else {
-				//CLOSE QUOTES
-				if(QuoteChar == str[x]) {
-					result+= '\'';
-					betweenQuotes = false;
-				}
-				//STILL INSIDE QUOTES
-				else
-					result+= '\\\'';
-			}
-
-		}
-		//IF ESCAPE CHARACTER IS FOUND AND DOUBLE OR SINGLE QUOTE AFTER
-		else if(str[x] === '\\' && (str[x+1] === '\'' || str[x+1] === '"')) {
-			//ESCAPE + SINGLE QUOTE
-			if(str[x+1] === '\'') {
-				result+= '\\\\\'';
+		} else if (str[x] === '\\' && (str[x + 1] === '\'' || str[x + 1] === '"')) {
+			// IF ESCAPE CHARACTER IS FOUND AND DOUBLE OR SINGLE QUOTE AFTER
+			// ESCAPE + SINGLE QUOTE
+			if (str[x + 1] === '\'') {
+				result += '\\\\\'';
 				x++;
-			}
-			//ESCAPE + DOUBLE QUOTE
-			else if(str[x+1] === '"') {
-				result+= '"';
+			} else if (str[x + 1] === '"') {
+				// ESCAPE + DOUBLE QUOTE
+				result += '"';
 				x++;
+			}			else				{
+				result += str[x];
 			}
-			else
-				result+= str[x];
-		}
-		//BACKSLASHES --> DON'T TOUCHE THEM
-		else if(str[x] === '\\' && str[x+1] === '\\') {
-			result+='\\\\';
+		}		else if (str[x] === '\\' && str[x + 1] === '\\') {
+			// BACKSLASHES --> DON'T TOUCHE THEM
+			result += '\\\\';
 			x++;
+		}		else			{
+			result += str[x];
 		}
-		else
-			result+= str[x];
 	}
 	return result;
 };
