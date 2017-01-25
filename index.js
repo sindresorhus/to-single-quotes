7f203d0f6a25ab5e74911732b77de02f8dc7eae2
Rui Neves <rpmrneves@gmail.com>
2017-01-25 19:10:57 +0000

    Use string processing instead of regex (#14)

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 'use strict';
 module.exports = function (str) {
-	return str.replace(/(?:\\*)?"([^"\\]*\\.)*[^"]*"/g, function (match) {
-		return match
-			// unescape double-quotes
-			.replace(/([^\\]|^)\\"/g, '$1"')
-			// escape escapes
-			.replace(/(^|[^\\])(\\+)'/g, '$1$2\\\'')
-			// escape single-quotes - round 1
-			.replace(/([^\\])'/g, '$1\\\'')
-			// escape single-quotes - round 2 (for consecutive single-quotes)
-			.replace(/([^\\])'/g, '$1\\\'')
-			// convert
-			.replace(/^"|"$/g, '\'');
-	});
+	var result = '';
+	var i;
+	var betweenQuotes = false;
+	var quoteChar;
+	for (i = 0; i < str.length; i++) {
+		// found double-quote or single-quote
+		if (str[i] === '"' || str[i] === '\'') {
+			// if not processing in between quotes
+			if (!betweenQuotes) {
+				quoteChar = str[i];
+				betweenQuotes = true;
+				result += '\'';
+			} else if (quoteChar === str[i]) {
+				// if processing between quotes
+				// close quotes
+				result += '\'';
+				betweenQuotes = false;
+			} else {
+				// still inside quotes
+				result += '\\\'';
+			}
+		} else if (str[i] === '\\' && (str[i + 1] === '\'' || str[i + 1] === '"')) {
+			// if escape character is found and double or single quote after
+			// escape + single-quote
+			if (str[i + 1] === '\'') {
+				result += '\\\\\'';
+				i++;
+			} else if (str[i + 1] === '"') {
+				// escape + double-quote
+				result += '"';
+				i++;
+			} else {
+				result += str[i];
+			}
+		} else if (str[i] === '\\' && str[i + 1] === '\\') {
+			// don't touch backslashes
+			result += '\\\\';
+			i++;
+		} else {
+			result += str[i];
+		}
+	}
+	return result;
 };
