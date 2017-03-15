592af0c4d69367343bb7f053b8699fcf8023a1f8
Rui Neves <rpmrneves@gmail.com>
2017-03-15 05:54:48 +0000

    Support different quotes (#16)

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 'use strict';
-module.exports = str => {
+
+function transform(input, toSingleQuotes) {
 	let result = '';
 	let betweenQuotes = false;
 	let quoteChar;
+	let changeTo = '';
+	let toChange = '';
 
-	for (let i = 0; i < str.length; i++) {
-		const current = str[i];
-		const next = str[i + 1];
+	if (toSingleQuotes) {
+		changeTo = '\'';
+		toChange = '"';
+	}	else {
+		changeTo = '"';
+		toChange = '\'';
+	}
+
+	for (let i = 0; i < input.length; i++) {
+		const current = input[i];
+		const next = input[i + 1];
 
 		// Found double-quote or single-quote
 		if (current === '"' || current === '\'') {
@@ -14,24 +25,36 @@ module.exports = str => {
 			if (!betweenQuotes) {
 				quoteChar = current;
 				betweenQuotes = true;
-				result += '\'';
+				result += changeTo;
 			} else if (quoteChar === current) {
 				// If processing between quotes, close quotes
-				result += '\'';
+				result += changeTo;
 				betweenQuotes = false;
 			} else {
 				// Still inside quotes
-				result += '\\\'';
+				result += '\\' + changeTo;
 			}
 		} else if (current === '\\' && (next === '\'' || next === '"')) {
 			// If escape character is found and double or single quote after
-			// Escape + single-quote
-			if (next === '\'') {
-				result += '\\\\\'';
+			// Escape + quote to change to
+			if (next === changeTo) {
+				// If in between quotes and quote is equal to changeTo only escape once
+				if (betweenQuotes && quoteChar === changeTo) {
+					result += '\\' + changeTo;
+				}	else {
+					// Not between quotes, escape twice
+					result += '\\\\' + changeTo;
+				}
 				i++;
-			} else if (next === '"') {
-				// Escape + double-quote
-				result += '"';
+			} else if (next === toChange) {
+				// Escape + quote to be changed
+				// If between quotes can mantain tochange
+				if (betweenQuotes) {
+					result += toChange;
+				}	else {
+					// Otherwise replace it by quote changeto
+					result += changeTo;
+				}
 				i++;
 			} else {
 				result += current;
@@ -46,4 +69,8 @@ module.exports = str => {
 	}
 
 	return result;
-};
+}
+
+module.exports = input => transform(input, true);
+
+module.exports._transform = transform;
